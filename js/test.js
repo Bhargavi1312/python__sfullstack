715598e9b2b2f56ebea5078b671a7d73742beb01
Bhargavi <bhargavitulimelli@gmail.com>
2026-07-28 10:37:04 +0000

    changes

---
+++ js/test.js
@@ -0,0 +1,33 @@
+console.log("Hello World")
+let student_name="Bhargavi"
+console.log("student name is:"+ student_name)
+let student_age=21
+let course_name="python full stack"
+console.log("student age is:"+
+student_age,"course name is:"+
+course_name)
+let fee=10000
+let discount=10
+let discounted_fee=fee-(fee*discount/100)
+console.log("Discounted fee is:"+ discounted_fee)
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")
+}
+else{
+    console.log("Not eligible for admission")
+}
+for(let i=1;i<=5;i++){
+        console.log("Iteration number:"+i)
+    }
+    const pi=3.14
+    console.log("value of pi is:+pi")
+    let student={
+        name:"devi",
+        age:20,
+        course:"python full stack",
+        fee:10000
+    }
+    console.log("Student details:",student)
+    function greetStudent(name){
+        console.log("Hello"+name+",welcome to NRiit Learning Management System")
